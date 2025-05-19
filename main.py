143c49d8499bdc991d10c8121c17a2b058e5d952
Dagset51 <jhudson51@outlook.com>
2025-05-19 14:25:28 -0600

    building lower case function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import count_words
+from stats import count_characters
+
 def get_book_text():
     with open("books/frankenstein.txt") as f:
         text = f.read()
@@ -8,6 +10,7 @@ def get_book_text():
 def main():
     text = get_book_text()
     num_words = count_words(text)
+    characters = count_characters(text)
     print(f"{num_words} words found in the document")
 
 main()
